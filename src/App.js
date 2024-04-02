c7da2ab0b0161aff4173299eb60f5faf2e37dd77
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2024-04-01 21:38:54 -0400

    Changed logo and CSS around image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
-import HomeV3 from "./components/Home/HomeV3";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
@@ -35,7 +35,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<HomeV3 />} />
+          <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
