4080e4b9f052987d4463cdf9b94bcca00b62fdd7
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2024-04-03 17:07:21 -0400

    Added projects without actual html

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,40 +1,32 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import Badge from "react-bootstrap/Badge";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
-      </Card.Body>
+      <div style={{ padding: '1rem' }}> {/* Wrapper div with padding */}
+        <Card.Body>
+          <Card.Title className="orange">
+            {props.title} | {props.date}
+          </Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
+          {props.tags && props.tags.map((tag, index) => (
+            <Badge key={index} pill variant="primary" className="mr-2">
+              {tag}
+            </Badge>
+          ))}
+        </Card.Body>
+        <div style={{ marginTop: 'auto', textAlign: 'center' }}> {/* Centering the button */}
+          <Button variant="primary" href={props.link} target="_blank">Explore</Button>
+        </div>
+      </div>
     </Card>
   );
 }
+
 export default ProjectCards;
